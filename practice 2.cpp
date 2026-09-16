f44e2d12d4c3248b6c94a7776bb3e3858b911192
CyrusOmbasa <ombasacyrus50@gmail.com>
2026-09-16 23:09:21 +0300

    Add files via upload

---
+++ practice 2.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+int main()
+{
+  int num;
+  std::cout<<"please user enter number";
+  std::cin>>num;
+
+  if(num==1){
+   std::cout<<"CYRUS";
+  }
+  else if(num==2){
+   std::cout<<"TRIZAH";
+  }
+  else if(num==3){
+   std::cout<<"CALVIN";
+  }
+  else{
+   std::cout<<"WRONG ENTRY";
+  }
+  
+   return 0;
+
+}
